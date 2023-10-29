45b90b97381c0738da168ffbdd0d296c0ec9994e
TechCoder-RM <ria.work2@gmail.com>
2023-10-29 15:45:16 -0400

    added some changes to fix errors

--- tests/app_test.py
+++ tests/app_test.py
@@ -71,17 +71,6 @@ def test_empty_db(client):
     assert b"No entries yet. Add some!" in rv.data
 
 
-def test_delete_message(client):
-    """Ensure the messages are being deleted"""
-    rv = client.get("/delete/1")
-    data = json.loads(rv.data)
-    assert data["status"] == 0
-    login(client, app.config["USERNAME"], app.config["PASSWORD"])
-    rv = client.get("/delete/1")
-    data = json.loads(rv.data)
-    assert data["status"] == 1
-
-
 def test_login_logout(client):
     """Test login and logout using helper functions"""
     rv = login(client, app.config["USERNAME"], app.config["PASSWORD"])
@@ -105,3 +94,13 @@ def test_messages(client):
     assert b"No entries here so far" not in rv.data
     assert b"&lt;Hello&gt;" in rv.data
     assert b"<strong>HTML</strong> allowed here" in rv.data
+
+def test_delete_message(client):
+    """Ensure the messages are being deleted"""
+    rv = client.get("/delete/1")
+    data = json.loads(rv.data)
+    assert data["status"] == 0
+    login(client, app.config["USERNAME"], app.config["PASSWORD"])
+    rv = client.get("/delete/1")
+    data = json.loads(rv.data)
+    assert data["status"] == 1
